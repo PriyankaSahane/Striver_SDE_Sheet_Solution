4510844864a855b05af3c13e296e5debef714ffc
Priyanka Sahane <118671024+PriyankaSahane@users.noreply.github.com>
2025-05-31 16:14:07 +0530

    Time: 1 ms (72.45%), Space: 45.5 MB (80.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -62,7 +62,7 @@ class Solution {
 	    }
     }*/
 
-    ArrayList<Integer> setRowList= new ArrayList<>();
+    /*ArrayList<Integer> setRowList= new ArrayList<>();
     ArrayList<Integer> setColList= new ArrayList<>();
 
     for(int i=0; i<matrix.length; i++){
@@ -102,5 +102,48 @@ class Solution {
             }
         }
         return inputMatrix;
+    }*/
+
+
+
+    int col0=1;
+    for(int i=0; i<matrix.length; i++){
+        for(int j=0; j<matrix[i].length; j++){
+            if(matrix[i][j]==0){
+                if(j!=0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }else{
+                    col0=0;
+                }
+
+                
+            }
+        }
+    }
+
+    for(int i =1; i<matrix.length; i++){
+        for(int j=1; j<matrix[i].length; j++){
+            if(matrix[i][j] != 0){
+                // check for col and row
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
+            }
+          
+        }
+      
+    }
+
+    if(matrix[0][0]==0){
+        for(int j=0; j<matrix[0].length; j++){
+            matrix[0][j]=0;
+        }
+    }
+    if(col0==0){
+        for(int i=0; i<matrix.length; i++){
+            matrix[i][0]=0;
+        }
+    }
     }
 }
