36c345b1126bffba4b8f07515d1174485cb31154
Priyanka Sahane <118671024+PriyankaSahane@users.noreply.github.com>
2025-06-19 21:57:12 +0530

    Time: 2 ms (10.55%), Space: 42.2 MB (32.86%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,19 @@
 class Solution {
-    public void sortColors(int[] nums) {
-      Arrays.sort(nums); 
+    public void sortColors(int[] arr) {
+
+        for(int i=0; i<arr.length-1; i++){
+            int min=i;
+            for(int j=i; j<arr.length; j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+                
+            }
+            int temp= arr[i];
+            arr[i]= arr[min];
+            arr[min]= temp;
+        }
+        
+        
     }
 }
